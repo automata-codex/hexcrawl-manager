c3a6b65a693659a7318aebcca63fd1891c8db282
Alex Gates-Shannon <alex.nebula99@gmail.com>
2024-12-22 09:37:25 -0500

    Add other hexes from region 8

--- schemas/hex-database.js
+++ schemas/hex-database.js
@@ -3,9 +3,9 @@ import { z } from 'zod';
 export const HexDataSchema = z.object({
  id: z.string(),
  name: z.string(),
- coordinates: z.tuple([z.string(), z.number()]),
+ coordinates: z.tuple([z.string(), z.number()]).optional(), // We can derive coordinates from the id, but this is still in the schema for backwards compatibility.
  landmark: z.string(),
- hiddenSites: z.array(z.string()),
+ hiddenSites: z.array(z.string()).optional(),
  regionId: z.string(),
  hideInCatalog: z.boolean().optional(),
 }).describe('Data for a hex in a hex map.');
