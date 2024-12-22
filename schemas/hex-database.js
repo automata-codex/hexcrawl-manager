99fcce6721112c77644bf577db53aa1bec259f48
Alex Gates-Shannon <alex.nebula99@gmail.com>
2024-12-21 19:02:10 -0500

    Flesh out hex database schema

--- schemas/hex-database.js
+++ schemas/hex-database.js
@@ -2,6 +2,10 @@ import { z } from 'zod';
 
 export const HexDataSchema = z.object({
  id: z.string(),
+ name: z.string(),
+ coordinates: z.tuple([z.string(), z.number()]),
+ landmark: z.string(),
+ hiddenSites: z.array(z.string()),
 }).describe('Data for a hex in a hex map.');
 
 export const HexDatabaseSchema = z.array(
