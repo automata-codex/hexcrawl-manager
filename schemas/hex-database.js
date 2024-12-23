d7bfa1189c8a5713b03ca1b83c1279d15476948c
Alex Gates-Shannon <alex.nebula99@gmail.com>
2024-12-23 13:32:11 -0500

    Add some multicultural sites

--- schemas/hex-database.js
+++ schemas/hex-database.js
@@ -1,17 +1,18 @@
 import { z } from 'zod';
 
 export const HexDataSchema = z.object({
- id: z.string(),
- name: z.string(),
- coordinates: z.tuple([z.string(), z.number()]).optional(), // We can derive coordinates from the id, but this is still in the schema for backwards compatibility.
- landmark: z.string(),
- hiddenSites: z.array(z.string()).optional(),
- regionId: z.string(),
- hideInCatalog: z.boolean().optional(),
- isVisited: z.boolean().optional(),
- isExplored: z.boolean().optional(),
+  id: z.string(),
+  name: z.string(),
+  coordinates: z.tuple([z.string(), z.number()]).optional(), // We can derive coordinates from the id, but this is still in the schema for backwards compatibility.
+  landmark: z.string(),
+  hiddenSites: z.array(z.string()).optional(),
+  secretSite: z.string().optional(),
+  regionId: z.string(),
+  hideInCatalog: z.boolean().optional(),
+  isVisited: z.boolean().optional(),
+  isExplored: z.boolean().optional(),
 }).describe('Data for a hex in a hex map.');
 
 export const HexDatabaseSchema = z.array(
-    HexDataSchema
+  HexDataSchema
 ).describe('Data for hexes in a hex map.');
