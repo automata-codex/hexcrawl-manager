3bd31eb169eeee0eef574f4956f980b19d67d10a
Alex Gates-Shannon <alex.nebula99@gmail.com>
2024-12-25 10:33:29 -0500

    Add data for regions 7 and 9

--- schemas/random-encounter-table.js
+++ schemas/random-encounter-table.js
@@ -3,10 +3,10 @@ import { z } from 'zod';
 export const RandomEncounterTableSchema = z.array(
   z.object({
     id: z.string(),
-    range: z.array([
+    range: z.array(
       z.number().int().min(1).max(20),
       z.number().int().min(1).max(20).optional(),
-    ]),
+    ),
     description: z.union([
       z.object({ text: z.string() }),
       z.object({ encounter: z.string() }),
