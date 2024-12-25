9a44f34931bfbe28924eb3098191989cfcd3fe23
Alex Gates-Shannon <alex.nebula99@gmail.com>
2024-12-25 11:18:41 -0500

    Display more data on the region pages

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 import { HexDataSchema } from '../schemas/hex-database.js';
-import type { RegionDataSchema } from '../schemas/region';
+import { RandomEncounterTableSchema } from '../schemas/random-encounter-table';
+import { RegionDataSchema } from '../schemas/region';
 
 export type HexData = z.infer<typeof HexDataSchema>;
+export type RandomEncounterTableData = z.infer<typeof RandomEncounterTableSchema>;
 export type RegionData = z.infer<typeof RegionDataSchema>;
