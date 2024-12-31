d3ed7ba49be4bc1b5964db24a090080db748d8c6
Alex Gates-Shannon <alex.nebula99@gmail.com>
2024-12-31 12:18:46 -0500

    Add pages for dungeons

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod';
+import { DungeonDataSchema } from '../schemas/dungeon';
 import { HexDataSchema } from '../schemas/hex-database.js';
 import { RandomEncounterSchema } from '../schemas/random-encounter';
 import { RandomEncounterTableSchema } from '../schemas/random-encounter-table';
 import { RegionDataSchema } from '../schemas/region';
 import { StatBlockSchema } from '../schemas/stat-block.js';
 
+export type DungeonData = z.infer<typeof DungeonDataSchema>;
 export type HexData = z.infer<typeof HexDataSchema>;
 export type RandomEncounterData = z.infer<typeof RandomEncounterSchema>;
 export type RandomEncounterTableData = z.infer<typeof RandomEncounterTableSchema>;
