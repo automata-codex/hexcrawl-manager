866386b66c442318b3b831f0e5313892b9446e0b
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-01-08 18:21:45 -0500

    Simplify props to `HexData` component; show GM notes in hex detail view; use smart quotation marks in rendered Markdown; add a function for using smarty quotes with regular text blurbs

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -2,10 +2,12 @@ import rehypeAddClasses from 'rehype-add-classes';
 import rehypeStringify from 'rehype-stringify';
 import { remark } from 'remark';
 import remarkRehype from 'remark-rehype';
+import remarkSmartypants from 'remark-smartypants';
 
-export async function processMarkdown(markdown: string) {
+export async function renderMarkdown(markdown: string) {
   const result = await remark()
     .use(remarkRehype) // Converts MDAST to HAST
+    .use(remarkSmartypants) // Adds typographic enhancements
     .use(rehypeAddClasses, {
       h1: 'title is-2',
       h2: 'title is-3',
---
+++ src/utils/text.ts
@@ -0,0 +1,14 @@
+import { unified } from 'unified';
+import retextSmartypants from 'retext-smartypants';
+import retextParse from 'retext-english';
+import retextStringify from 'retext-stringify';
+
+export async function formatText(text: string) {
+  const file = await unified()
+    .use(retextParse) // Parse the text
+    .use(retextSmartypants) // Apply typographic transformations
+    .use(retextStringify) // Convert the processed tree back to plain text
+    .process(text);
+
+  return String(file);
+}
