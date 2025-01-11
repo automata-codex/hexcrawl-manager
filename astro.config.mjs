8aa484d58226835401a16b7985427a29956ae688
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-01-11 08:47:32 -0500

    Add authentication with Clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,10 @@
+import vercel from '@astrojs/vercel';
+import clerk from '@clerk/astro'
 import { defineConfig } from 'astro/config';
-import rehypeAddClasses from 'rehype-add-classes';
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    rehypePlugins: [
-      [
-        rehypeAddClasses,
-        {
-          h1: 'title is-2',
-          h2: 'title is-3',
-          h3: 'title is-4',
-          h4: 'title is-5',
-        },
-      ],
-    ],
-  },
-  vite: {
-    plugins: []
-  }
+  adapter: vercel(),
+  integrations: [clerk()],
+  output: 'server',
 });
