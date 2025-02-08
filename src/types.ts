27f78e52537a656abdc0f9c671fcdea4aea4851d
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-02-08 09:09:26 -0500

    Add session page and component

--- src/types.ts
+++ src/types.ts
@@ -7,6 +7,7 @@ import { RandomEncounterSchema } from '../schemas/random-encounter';
 import { RandomEncounterTableSchema } from '../schemas/random-encounter-table';
 import { RegionSchema } from '../schemas/region';
 import { ScopeSchema } from '../schemas/scopes';
+import { SessionSchema } from '../schemas/session';
 import {
   DescriptiveActionSchema,
   MeleeWeaponAttackSchema,
@@ -26,6 +27,7 @@ export type RandomEncounterTableData = z.infer<typeof RandomEncounterTableSchema
 export type RangedWeaponAttackData = z.infer<typeof RangedWeaponAttackSchema>;
 export type RegionData = z.infer<typeof RegionSchema>;
 export type Scope = z.infer<typeof ScopeSchema>;
+export type SessionData = z.infer<typeof SessionSchema>;
 export type StatBlockData = z.infer<typeof StatBlockSchema>;
 export type StatBlockSkillsData = z.infer<typeof SkillsSchema>;
 
