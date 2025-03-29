a2e2b5c0afc03f0eb44da36f16044e018b060626
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-03-29 16:50:58 -0400

    Add initial data for region 17

--- src/utils/id-parsers.ts
+++ src/utils/id-parsers.ts
@@ -6,7 +6,18 @@ export function getHexRow(hexId: string): number {
   return parseInt(hexId.substring(1), 10);
 }
 
-export function getRegionTitle(regionId: string): string {
+export function getRegionNumber(regionId: string): number {
   const regionNumber = regionId.split('-')[1];
+  return parseInt(regionNumber, 10);
+}
+
+export function getRegionTitle(regionId: string): string {
+  const regionNumber = getRegionNumber(regionId);
   return `Region ${regionNumber}`;
+}
+
+export function regionIdCompare(a: string, b: string): number {
+  const aRegionNumber = getRegionNumber(a);
+  const bRegionNumber = getRegionNumber(b);
+  return aRegionNumber - bRegionNumber;
 }
