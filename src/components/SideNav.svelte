02ef98a74760d24077819da3a071a7d51f9054eb
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-04-01 09:54:19 -0400

    Install Svelte and create a basic menu

---
+++ src/components/SideNav.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let open = false;
+</script>
+
+<button on:click={() => open = !open}>☰ Toggle Menu</button>
+
+{#if open}
+  <aside>
+    <nav>
+      <ul>
+        <li><a href="/heritage">Heritage</a></li>
+        <li><a href="/regions">Regions</a></li>
+      </ul>
+    </nav>
+  </aside>
+{/if}
