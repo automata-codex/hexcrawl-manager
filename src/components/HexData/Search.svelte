95d6642fdccf6b0779d39cea1fb4cf0cc2a51e33
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-04-17 22:22:55 -0400

    Try to make the Svelte component interact with the DOM

---
+++ src/components/HexData/Search.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  interface Props {
+    onSearch: (value: string) => void;
+  }
+  const props = $props();
+
+  let query = $state('');
+
+  function handleInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    console.log(`>> input: ${target.value}`);
+    props.onSearch?.(target.value);
+    console.log(`>> onSearch: ${!!props.onSearch}`);
+  }
+</script>
+
+<input
+  type="text"
+  bind:value={query}
+  oninput={handleInput}
+  placeholder="Search hexes..."
+  class="input"
+/>
