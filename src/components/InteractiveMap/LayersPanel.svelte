507fef5f2ba5718032627032ca928c245d064428
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-05-06 16:54:19 -0400

    Add slide-in panel for layers

--- src/components/InteractiveMap/LayersPanel.svelte
+++ src/components/InteractiveMap/LayersPanel.svelte
@@ -10,7 +10,31 @@
   <FontAwesomeIcon icon={faLayerGroup} />
 </button>
 
+<!-- Sliding Panel -->
+<aside class:open={isOpen} class="layers-panel">
+</aside>
+
 <style>
+    .layers-panel {
+        position: fixed;
+        top: 1rem;
+        right: 1rem;
+        width: 250px;
+        background-color: var(--bulma-body-background-color);
+        box-shadow: 2px 0 5px rgba(0,0,0,0.5);
+        transform: translateX(+200%);
+        transition: transform 0.3s ease;
+        z-index: 1000;
+        overflow-y: auto;
+        margin-bottom: 0;
+        padding: 0.5rem;
+        border-radius: 0.5rem;
+    }
+
+    .layers-panel.open {
+        transform: translateX(0%);
+    }
+
     .open-panel-button {
         position: absolute;
         top: 1rem;
