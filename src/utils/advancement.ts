ee4b587f8cfd14f36e9cc1c762299b6063a02e10
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-05-14 20:56:22 -0400

    Fix bug in progress meter

--- src/utils/advancement.ts
+++ src/utils/advancement.ts
@@ -8,3 +8,50 @@ export function getPointsPerLevel(level: number): number {
   }
   throw new Error(`Invalid level: ${level}`);
 }
+
+export function getPointsToReachLevel(level: number): number {
+  switch (level) {
+    case 1:
+      return 0;
+    case 2:
+      return 2;
+    case 3:
+      return 4;
+    case 4:
+      return 6;
+    case 5:
+      return 10;
+    case 6:
+      return 14;
+    case 7:
+      return 18;
+    case 8:
+      return 22;
+    case 9:
+      return 26;
+    case 10:
+      return 30;
+    case 11:
+      return 36;
+    case 12:
+      return 42;
+    case 13:
+      return 48;
+    case 14:
+      return 54;
+    case 15:
+      return 60;
+    case 16:
+      return 66;
+    case 17:
+      return 72;
+    case 18:
+      return 78;
+    case 19:
+      return 84;
+    case 20:
+      return 90;
+    default:
+      throw new Error(`Invalid level: ${level}`);
+  }
+}
