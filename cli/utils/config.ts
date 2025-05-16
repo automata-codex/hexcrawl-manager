cdd7e461d7c0cbb62e10a8c1ab95d015ef9424b1
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-05-16 16:53:07 -0400

    Implement "add trail" logic

--- cli/utils/config.ts
+++ cli/utils/config.ts
@@ -1,6 +1,10 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { z } from 'zod';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const ConfigSchema = z.object({
   repoRoot: z.string().min(1)
