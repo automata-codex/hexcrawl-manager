82df36f992df640dbf14b8f4e0aac0d0896d2ac0
Alex Gates-Shannon <alex.nebula99@gmail.com>
2025-09-28 10:48:30 -0400

    Fix recursive type so `@skyreach/schemas` will build

--- packages/schemas/src/schemas/knowledge-node.ts
+++ packages/schemas/src/schemas/knowledge-node.ts
@@ -1,17 +1,26 @@
 import { z } from 'zod';
 
-export const KnowledgeNodeSchema = z.object({
-  id: z
-    .string()
-    .regex(/^[a-z0-9_]+$/, 'ID must be lowercase with underscores only'),
-  children: z.lazy(() => z.array(KnowledgeNodeSchema)).optional(),
-  description: z.string(),
-  isKnown: z.boolean().optional().default(false),
-  name: z.string(),
-  details: z
-    .string()
-    .optional()
-    .describe('Further details about the clue; extended information'),
-});
+export type KnowledgeNodeData = {
+  id: string;
+  children?: KnowledgeNodeData[];
+  description: string;
+  isKnown?: boolean;
+  name: string;
+  details?: string;
+};
 
-export type KnowledgeNodeData = z.infer<typeof KnowledgeNodeSchema>;
+export const KnowledgeNodeSchema: z.ZodType<KnowledgeNodeData> = z.lazy(() =>
+  z.object({
+    id: z
+      .string()
+      .regex(/^[a-z0-9_]+$/, 'ID must be lowercase with underscores only'),
+    children: z.array(KnowledgeNodeSchema).optional(),
+    description: z.string(),
+    isKnown: z.boolean().optional(),
+    name: z.string(),
+    details: z
+      .string()
+      .optional()
+      .describe('Further details about the clue; extended information'),
+  })
+);
